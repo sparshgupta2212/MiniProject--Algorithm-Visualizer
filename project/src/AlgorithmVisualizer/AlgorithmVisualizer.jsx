29e818ddfe7e1b511bbc846fdb8585dc4c123dc1
Siddharth Singh <45232466+siddharth1806@users.noreply.github.com>
2020-09-05 01:05:05 +0530

    Update AlgorithmVisualizer.jsx

--- project/src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
+++ project/src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 function arraynum(min, max) {
-
-  return Math.floor(Math.random() * (max - min + 1) + min);
+return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export default class SortingVisualizer extends React.Component {
