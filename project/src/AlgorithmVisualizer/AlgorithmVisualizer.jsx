26df374140e90ae2152db3b4bc2fb6b7282c9c55
Siddharth Singh <45232466+siddharth1806@users.noreply.github.com>
2020-09-05 01:09:43 +0530

    Update AlgorithmVisualizer.jsx

--- project/src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
+++ project/src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
@@ -5,8 +5,7 @@ return Math.floor(Math.random() * (max - min + 1) + min);
 export default class SortingVisualizer extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
+  this.state = {
       arr: [],
     };
   }
