9587647dce7fb03eb65f1972768435613bac1243
Siddharth Singh <45232466+siddharth1806@users.noreply.github.com>
2020-09-05 01:03:32 +0530

    Update App.js

--- project/src/App.js
+++ project/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import AlgorithmVisualizer from './AlgorithmVisualizer/AlgorithmVisualizer';
-
 function App() {
   return (
     <div className="App">
